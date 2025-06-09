5db667fd0c3f891843098f09540ce736aab84106
Svetlana_Shcherbakova <shchalal@mail.ru>
2025-06-09 20:42:35 +0300

    no message

---
+++ event-object/tabs/task.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('.tab__navigation').forEach((nav) => {
+  
+  const tabs = Array.from(nav.querySelectorAll('.tab')); 
+  const contents = nav.nextElementSibling.querySelectorAll('.tab__content');
+
+  tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+      tabs.forEach(t => t.classList.remove('tab_active'));
+      contents.forEach(c => c.classList.remove('tab__content_active'));
+
+      tab.classList.add('tab_active');            
+      contents[index].classList.add('tab__content_active');
+    });
+  });
+});
